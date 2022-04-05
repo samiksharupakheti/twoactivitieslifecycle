7cf8261f2f96f67764be8924b1f426f03394d588
samiksharupakheti <samiksharupakheti@gmail.com>
2022-04-05 23:25:48 +0545

    adding the homework for the twoactivity lifecycle

--- app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
+++ app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
@@ -1,77 +1,43 @@
 package com.example.twoactivitieslifecycle;
 
-import android.content.Intent;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
-    public static final int TEXT_REQUEST = 1;
-    private ShopList items = new ShopList();
-    public MainActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = getIntent();
-        if ((savedInstanceState != null)&& (savedInstanceState.getSerializable("list")!= null)){
-            HashMap<String, Integer> l = (HashMap<String, Integer>)savedInstanceState.getSerializable("list");
-            TextView tv = findViewById(R.id.textView);
-            tv.setText("");
-            for (String k : l.keySet()) {
-                String s = l.get(k).toString() + " " + k + "\n";
-                tv.setText(tv.getText() + s);
-                for(int i = 0; i<l.get(k); i++){
-                    items.addItem(k);}
-            }
+        mShowCount = (TextView) findViewById(R.id.view_count);
+        if (savedInstanceState != null) {
+            mCount = savedInstanceState.getInt("count");
+            mShowCount.setText("" + mCount);
         }
+
     }
 
-    public void launchSecondActivity(View view) {
-
-        Intent intent = new Intent(this, SecondActivity.class);
-
-        startActivityForResult(intent, TEXT_REQUEST);
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putSerializable("list", items.getItems());
-    }
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        EditText text = findViewById(R.id.EditText);
+        CharSequence data = text.getText();
+        outState.putCharSequence("data", data);
+        outState.putInt("count", mCount);
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String item = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
-                items.addItem(item);
-            }
-            drawView();
-        }
-    }
-
-    public void drawView() {
-        HashMap<String, Integer> l = items.getItems();
-        TextView tv = findViewById(R.id.textView);
-        tv.setText("");
-        for (String k : l.keySet()) {
-            String s = l.get(k).toString() + " " + k + "\n";
-            tv.setText(tv.getText() + s);
-        }
     }
 }
